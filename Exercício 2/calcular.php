c397f2a2d7456c45b2708ed53183194cabdbe676
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-15 16:41:28 -0300

    f 0.2

---
+++ Exercício 2/calcular.php
@@ -0,0 +1,16 @@
+<?php
+// Declaração das variáveis
+    $num1 = $_POST ['num1'];
+    $num2 = $_POST ['num2'];
+    $num3 = $_POST ['num3'];
+    $num4 = $_POST ['num4'];
+    $media = ($num1 + $num2 + $num3  + $num4)/4;
+    
+// Declaração do if
+    if ($media > 5)
+    { echo "O aluno está aprovado com média igual a $media" }
+    else
+    {echo "O aluno está reprovado com média igual a $média" }
+
+
+?>
