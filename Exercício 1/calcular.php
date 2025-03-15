9c2d5dbe33eb2a01527def07e45ed59d4d884e72
Caua-Alborghette <cauaalborghette09@gamil.com>
2025-03-15 17:11:47 -0300

    C0 7

--- Exercício 1/calcular.php
+++ Exercício 1/calcular.php
@@ -3,10 +3,10 @@ $num1 = $_POST ['num1'];
         $num2 = $_POST ['num2'];
 
         if ($num1 > $num2)
-        { echo "$num1 é maior que $num2";}
+        { echo "$num1 é maior que $num2;";}
         else if ($num2 > $num1)
-        { echo "$num2 é maior que $num1"; }
+        { echo "$num2 é maior que $num1;"; }
         else
-        {echo "$num2 é igual $num1"; }
+        {echo "$num2 é igual $num1;"; }
 
 ?>
