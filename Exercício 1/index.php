afdb20311eded284cebd800984e5b19ed773ee31
Caua-Alborghette <cauaalborghette09@gamil.com>
2025-03-15 16:20:49 -0300

    C 03

--- Exercício 1/index.php
+++ Exercício 1/index.php
@@ -3,22 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercício 1</title>
 </head>
 <body>
+<h1>Calculadora:</h1>
 
-<h1>Calculadora:</h2>
+<form method = "post" action="calcular.php">
 
-    <form method = "post" action="variaveis.php">
+    <h2>Insira o primeiro número</h2>
+    <input type = "text" name="num1" size="25" /> <br></br>
 
-        <h2>Insira o primeiro número</h2>
-        <input type = "text" name="num1" size="25" /> <br/></br>
+    <h2>Insira o segundo número</h2>
+    <input type = "text" name="num2" size="25" /> <br></br>
 
-        <h2>Insira o segundo número</h2>
-        <input type = "text" name="num2" size="25" /> <br/></br>
-
-        <input type="submit">
-    </form>
-
+    <input type="submit">
+</form>
 </body>
 </html>
