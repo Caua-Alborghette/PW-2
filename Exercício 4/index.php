74a89125723b59c997dd13ec29eb1a855b9a16b5
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-15 19:20:06 -0300

    f 0.7

---
+++ Exercício 4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+</head>
+<body>
+<h1>Cálculo de média da nota:</h1>
+
+<form method = "post" action="calcular.php">
+
+    <h2>Insira a o peso:</h2>
+    <input type = "text, font famiy: comic sans"  name="peso" size="25" /> <br></br>
+
+    <h2>Insira a altura</h2>
+    <input type = "text" name="alt" size="25" /> <br></br>
+
+    <input type="submit">
+</form>
+</body>
+</html>
