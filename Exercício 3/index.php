5e4ec3ddc60cc9b842b071ad791bedc1644bfcc9
Caua-Alborghette <cauaalborghette09@gamil.com>
2025-03-15 16:56:27 -0300

    C 04

---
+++ Exercício 3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+<form method = "post" action="calcular.php">
+
+<h2>Insira o primeiro número</h2>
+<input type = "text" name="num1" size="25" /> <br></br>
+
+<h2>Insira o segundo número</h2>
+<input type = "text" name="num2" size="25" /> <br></br>
+
+<input type="submit">
+</body>
+</html>
