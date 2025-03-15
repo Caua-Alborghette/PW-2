229a4d9c400c6c94030ff76dae6b6182add269a9
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-15 19:29:09 -0300

    f 0.8

---
+++ Exercício 4/imc.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cálculo de IMC</title>
+</head>
+<body>
+</body>
+</html>
+
+<?php
+// Declaração das variáveis
+    $peso = $_POST ['peso'];
+    $alt = $_POST ['alt'];
+    $imc = $peso / $alt²;
+    
+// Declaração do if
+    if ($imc < 16){ 
+        echo "Seu IMC é $imc, você está com  magreza grave"; 
+    }
+
+    else if ($imc = 16 or $imc < 16.9){
+        echo "Seu IMC é $imc, você está com magreza moderada"; 
+        }
+?>
