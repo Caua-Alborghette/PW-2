6c3349e786403feae3c2b92f3eae75226d182830
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-15 19:45:35 -0300

    f 1.3

--- Exercício 4/imc.php
+++ Exercício 4/imc.php
@@ -23,4 +23,28 @@
     else if ($imc = 16 or $imc < 16.9){
         echo "Seu IMC é $imc, você está com magreza moderada"; 
         }
+
+    else if ($imc = 17 or $imc < 18.5){
+        echo "Seu IMC é $imc, você está com magreza leve"; 
+    }
+
+    else if ($imc = 18.5 or $imc < 24.9){
+        echo "Seu IMC é $imc, você está com um peso normal"; 
+    }
+
+    else if ($imc = 25 or $imc < 29.9){
+        echo "Seu IMC é $imc, você está com sobrepeso"; 
+    }
+
+    else if ($imc = 30 or $imc < 34.9){
+        echo "Seu IMC é $imc, você está com obesidade grau I"; 
+    }
+
+    else if ($imc = 35 or $imc < 39.9){
+        echo "Seu IMC é $imc, você está com obesidade grau II"; 
+    }
+
+    else {
+        echo "Seu IMC é $imc, você está com obesidade grau III ou mórbida"; 
+    }
 ?>
