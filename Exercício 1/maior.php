48bd09f30488cb47f33f41c23914c0988660cb68
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-15 22:45:03 -0300

    f 2.4

--- Exercício 1/maior.php
+++ Exercício 1/maior.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercício 1</title>
+        <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+        
+</body>
+</html>
 <?php
         $num1 = $_POST ['num1'];
         $num2 = $_POST ['num2'];
--- Exercício 2/calcular.php
+++ Exercício 2/calcular.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+    <link href="styles.css" rel="stylesheet" >
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 // Declaração das variáveis
     $num1 = $_POST ['num1'];
