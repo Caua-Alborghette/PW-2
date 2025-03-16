92a7aeeb44955815a01b46940eced71ad413500e
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-16 12:18:30 -0300

    f 2.6

--- Exercício 1/maior.php
+++ Exercício 1/maior.php
@@ -7,6 +7,16 @@
         <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
+<?php
+/*** set the content type header ***/
+/*** Without this header, it wont work ***/
+header("Content-type: text/css");
+
+
+$font_family = 'Arial, Helvetica, sans-serif';
+$font_size = '0.7em';
+$border = '1px solid';
+?>
         
 </body>
 </html>
